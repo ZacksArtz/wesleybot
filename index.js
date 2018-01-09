30546d2fdd067c9f2e7ab0f608260ad5efec544d
ZackArtz <zackmyers808@gmail.com>
2018-01-08 16:58:59 -1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+  });
